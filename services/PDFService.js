9c5351df9bf81ac6dd8eaf3e84d9180610d2daff
Jignesh Prajapati <jigneshprajapati22@gmail.com>
2025-04-16 10:55:08 +0530

    integrate docuseal api

--- services/PDFService.js
+++ services/PDFService.js
@@ -1,11 +1,48 @@
-const db = require('../config/db');
+const db = require("../config/db");
+const axios = require("axios");
+require('dotenv').config();
 const Log = db.log;
 
 class PDFService {
-    static async SaveFilter(reqBody) {
-        const response = await Log.create({ jobid: reqBody.jobId, userid: reqBody.userId, jsoncontent: JSON.stringify(reqBody) });
-        return response.data;
-    }
+  static async SaveFilter(reqBody) {
+
+    const response = await Log.create({
+      jobid: reqBody.jobId,
+      userid: reqBody.userId,
+      jsoncontent: JSON.stringify(reqBody),
+    });
+    return response.data;
+  }
+
+  static async UploadTemplateToDocuSeal(documents) {
+
+    const response = await axios.post(
+      `https://api.docuseal.com/templates/pdf`,
+      documents,
+      {
+        headers: {
+          "X-Auth-Token": process.env.DOCUSEAL_API_KEY,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    return response;
+  }
+
+  static async SendRequestForSignDocument(signRequest) {
+    const response = await axios.post(
+        `https://api.docuseal.com/submissions`,
+        signRequest,
+        {
+          headers: {
+            "X-Auth-Token": process.env.DOCUSEAL_API_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    return response;
+  }
 }
 
 module.exports = PDFService;
