a101d8b5c497725a6ae4e349917a88a54f71e15b
Nidhey-numlytics <nidhey.shah@numlytics.com>
2025-04-18 14:01:28 +0530

    e-sgn integrated

--- services/PDFService.js
+++ services/PDFService.js
@@ -1,4 +1,6 @@
-const db = require('../config/db');
+const db = require("../config/db");
+const axios = require("axios");
+require('dotenv').config();
 const Log = db.log;
 
 class PDFService {
@@ -6,6 +8,36 @@ class PDFService {
         const response = await Log.create({ jobid: reqBody.jobId, userid: reqBody.userId, jsoncontent: JSON.stringify(reqBody) });
         return response.data;
     }
+
+    static async UploadTemplateToDocuSeal(documents) {
+
+        const response = await axios.post(
+          `https://api.docuseal.com/templates/pdf`,
+          documents,
+          {
+            headers: {
+              "X-Auth-Token": process.env.DOCUSEAL_API_KEY,
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+          }
+        );
+        return response;
+      }
+    
+      static async SendRequestForSignDocument(signRequest) {
+        const response = await axios.post(
+            `https://api.docuseal.com/submissions`,
+            signRequest,
+            {
+              headers: {
+                "X-Auth-Token": process.env.DOCUSEAL_API_KEY,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        return response;
+      }
 }
 
 module.exports = PDFService;
